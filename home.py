54d4e3207f39d93f46326e47f6668a1d5ac85f63
Firsttt13 <firsthoodbaby885@gmail.com>
2024-11-19 14:26:31 +0700

    update

--- home.py
+++ home.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-st.title("website Deverloping using Python")
+st.title("🎶website Deverloping using Python🎶")
 st.header("website Deverloping using Python")
 
 st.subheader("Percc")
